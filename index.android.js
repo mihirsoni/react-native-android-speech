78b821f06c2497c129039ab82f77829286e8f702
Mihir Soni <m.soni@quikr.com>
2015-11-08 00:48:17 +0530

    Proper error handlings , returning proper erorr messages, removing consoles

--- index.android.js
+++ index.android.js
@@ -2,7 +2,7 @@
 * @Author: mihir
 * @Date:   2015-11-07
 * @Last Modified by:   mihir
-* @Last Modified time: 2015-11-07
+* @Last Modified time: 2015-11-08
 */
 
 'use strict';
@@ -11,47 +11,57 @@ var AndroidTTS = require('react-native').NativeModules.AndroidTTS;
 var RNAndroidTTS = {
   speak(args) {
     return new Promise((resolve, reject) => { 
-      AndroidTTS.speak(args, (isSpeaking) =>{
-        console.log(isSpeaking);
-        if(typeof isSpeaking === 'boolean'){
-          if (!isSpeaking) {
+      AndroidTTS.speak(args, (error,result) =>{
+          if (error) {
             reject(error);
+          } else {
+          	resolve(result);
           }
-            resolve(true);  
-        } else {
-          reject(isSpeaking);
-        }
-      });
+        });
     });
   },
   isSpeaking(){
     return new Promise((resolve,reject) => {
-      AndroidTTS.isSpeaking((result) => {
-        resolve(result);
+      AndroidTTS.isSpeaking((error,result) => {
+      	if(error) {
+      		reject(error)
+      	} else {
+      		resolve(result);	
+      	}
       });
     })
   },
   shutDown(){
   	return new Promise((resolve,reject) => {
-  		AndroidTTS.shutDown((result)=>{
-  			resolve(result)
+  		AndroidTTS.shutDown((error,result)=>{
+  			if(error) {
+      			reject(error)
+      		} else {
+      			resolve(result);	
+      		}
   		});
   	});
   },
   stop(){
   	return new Promise((resolve,reject) => {
-  		AndroidTTS.stop((result)=>{
-  			resolve(result)
+  		AndroidTTS.stop((error,result)=>{
+  			if(error) {
+      			reject(error)
+      		} else {
+      			resolve(result);	
+      		}
   		});
   	});
   },
   getLocales() {
     return new Promise((resolve, reject) => {
-      AndroidTTS.getLocale((error, locales) =>{
+      AndroidTTS.getLocale((error, results) =>{
         if (error) {
            reject(error);
+        } else {
+        	resolve(results);	
         }
-        resolve(locales);
+        
       });
     });
   }
